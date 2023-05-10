db720ccbf45acae3fdec621f52199b56bb051586
LinusConradM <linusconradm@gmail.com>
2023-05-10 04:38:41 +0000

    Correction

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,7 @@
 import requests
 
 
-def top_ten<subreddit>:
+def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
@@ -18,4 +18,4 @@ def top_ten<subreddit>:
         print("None")
         return
     results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get<"children">]
+    [print(c.get("data").get("title")) for c in results.get("children")]
